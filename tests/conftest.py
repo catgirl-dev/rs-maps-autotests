95ddef166626e3c9c1d1a236965bcced532a5379
catgirl-dev <183555309+catgirl-dev@users.noreply.github.com>
2025-11-04 01:25:37 +0300

    refactoring

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,12 @@ from utils.checks import Checking
 
 @pytest.fixture
 def created_location():
-    """Фикстура для создания тестовой локации"""
+    """Фикстура для создания локации и возвращения её place_id"""
+    print("\nPOST: создание локации (предусловие)")
+    post_result: Response = RsMapsApi.create_location()
+    check_post_result = post_result.json()
+    place_id = check_post_result.get("place_id")
+    Checking.check_status_code(post_result, 200)
+    Checking.check_json_token(post_result,
+                              ['status', 'place_id', 'scope', 'reference', 'id'])
+    yield place_id
--- tests/test_rs_maps_api.py
+++ tests/test_rs_maps_api.py
@@ -3,17 +3,12 @@ from requests import Response
 from utils.api import RsMapsApi
 from utils.checks import Checking
 
+
+
 class TestLocationLifecycle:
     """Проверка жизненного цикла локации: создание -> чтение -> обновление -> удаление"""
     def test_location_lifecycle(self, created_location):
-        print("\nPOST: создание локации")
-        post_result: Response = RsMapsApi.create_location()
-        check_post_result = post_result.json()
-        place_id = check_post_result.get("place_id")
-        Checking.check_status_code(post_result, 200)
-        Checking.check_json_token(post_result,
-                                  ['status', 'place_id', 'scope', 'reference', 'id'])
-
+        place_id = created_location
         print("\nGET: проверка, создалась ли локация")
         get_result: Response = RsMapsApi.get_location(place_id)
         Checking.check_status_code(get_result, 200)
@@ -41,3 +36,96 @@ class TestLocationLifecycle:
         Checking.check_json_token(get_result, ["msg"])
 
         print("Тестирование жизненного цикла локации прошло успешно!")
+
+
+
+class TestLocationNegative:
+    """Негативное тестирование"""
+    def test_get_location_without_place_id(self):
+        print("\nGET: попытка получить локацию с пустой строкой вместо place_id")
+        get_result = RsMapsApi.get_location("")
+        Checking.check_status_code(get_result, 404)
+        print("Негативное тестирование на получение локации с пустой строкой в place_id прошло успешно!")
+
+    def test_update_location_without_place_id(self):
+        print("\nPUT: попытка обновить локацию с пустой строкой вместо place_id")
+        update_result = RsMapsApi.put_location("")
+        Checking.check_status_code(update_result, 404)
+        print("Негативное тестирование на изменение локации с пустой строкой в place_id прошло успешно!")
+
+    def test_delete_location_without_place_id(self):
+        print("\nDELETE: попытка удалить локацию с пустой строкой вместо place_id")
+        delete_result = RsMapsApi.delete_location("")
+        Checking.check_status_code(delete_result, 404)
+        print("Негативное тестирование на получение локации с пустой строкой в place_id прошло успешно!")
+
+    def test_get_nonexistent_location(self):
+        print("\nGET: попытка получить несуществующую локацию")
+        fake_place_id = "0000000000"
+        get_result: Response = RsMapsApi.get_location(fake_place_id)
+        Checking.check_status_code(get_result, 404)
+        print("Негативное тестирование на получение несуществующей локации прошло успешно!")
+
+    def test_delete_nonexistent_location(self):
+        print("\nDELETE: попытка удалить несуществующую локацию")
+        fake_place_id = "0000000000"
+        delete_result: Response = RsMapsApi.delete_location(fake_place_id)
+        Checking.check_status_code(delete_result, 404)
+        print("Негативное тестирование на удаление несуществующей локации прошло успешно!")
+
+
+
+class TestLocationLocal:
+    """Тестирование отдельных операций с локацией"""
+    def test_only_create_location(self, created_location):
+        print("\nGET: проверка, создалась ли локация")
+        place_id = created_location
+        get_result: Response = RsMapsApi.get_location(place_id)
+        Checking.check_status_code(get_result, 200)
+        Checking.check_json_token(get_result, ['location', 'accuracy', 'name',
+                                                'phone_number', 'address', 'types', 'website', 'language'])
+        print("Тестирование только создания локации прошло успешно!")
+
+    def test_only_get_location(self, created_location):
+        place_id = created_location
+        print("\nGET: проверка только получения локации по существующему place_id")
+        get_result: Response = RsMapsApi.get_location(place_id)
+        Checking.check_status_code(get_result, 200)
+        Checking.check_json_token(get_result, [['location', 'name', 'address']])
+        print("Тестирование только получения локации прошло успешно!")
+
+    def test_only_change_location(self, created_location):
+        place_id = created_location
+        print("\nPUT: изменение адреса локации")
+        put_result: Response = RsMapsApi.put_location(place_id)
+        Checking.check_status_code(put_result, 200)
+        Checking.check_json_token(put_result, ["msg"])
+
+        print("\nGET: проверка, изменился ли адрес локации")
+        get_result: Response = RsMapsApi.get_location(place_id)
+        Checking.check_status_code(get_result, 200)
+        Checking.check_json_token(get_result, [['location', 'name', 'address']])
+        print("Тестирование только создания локации прошло успешно!")
+
+    def test_only_delete_location(self, created_location):
+        place_id = created_location
+        print("\nDELETE: удаление локации")
+        delete_result: Response = RsMapsApi.delete_location(place_id)
+        Checking.check_status_code(delete_result, 200)
+        Checking.check_json_token(delete_result, ["status"])
+
+        print("\nGET: проверка, удалилась ли локация")
+        get_result: Response = RsMapsApi.get_location(place_id)
+        Checking.check_status_code(get_result, 404)
+        Checking.check_json_token(get_result, ["msg"])
+        print("Тестирование только удаления локации прошло успешно!")
+
+
+
+
+
+
+
+
+
+
